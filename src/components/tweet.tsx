9d185c158ab7254ec36da190f4230443a6a60fac
loveAlakazam <dmsrkd1216@gmail.com>
2024-08-28 00:23:59 +0900

    :sparkles: Add: 트위터업데이트(텍스트 트위터만) 추가

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,8 +1,9 @@
 import { styled } from "styled-components";
 import { ITweet } from "./timeline";
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   display: grid;
@@ -38,13 +39,101 @@ const DeleteButton = styled.button`
   border: 0;
   font-size: 12px;
   padding: 5px 10px;
+  margin: 0px 10px;
   text-transform: uppercase;
   border-radius: 5px;
   cursor: pointer;
 `;
 
+// 수정폼 활성화
+const EditButton = styled.button`
+  background-color: #7f8689;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+// 수정폼
+const EditTweetFormTextArea = styled.textarea`
+  background-color: black;
+  border-radius: 10px;
+  color: white;
+  font-size: 16px;
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  resize: none;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+// 수정버튼
+const UpdateButton = styled.button`
+  background-color: #1d9bf0;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+// 수정취소버튼
+const CancelButton = styled.button`
+  background-color: #7f8689;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  margin: 0px 10px;
+  cursor: pointer;
+`;
+
 export default function Tweet({ userName, photo, tweet, userId, id }: ITweet) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTweet, setEditedTweet] = useState(tweet);
+
   const user = auth.currentUser;
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEditedTweet(e.target.value);
+  };
+  const handleCancel = () => {
+    // isEditing 값: true -> false 로 변경
+    setIsEditing(false);
+  };
+  const handleEdit = async () => {
+    // isEditing 값: false -> true 로 변경
+    setIsEditing(true);
+  };
+  const onUpdate = async () => {
+    try {
+      console.log("updated!");
+      // tweet 업데이트
+      await updateDoc(doc(db, "tweets", id), { tweet: editedTweet });
+
+      // tbd: 사진도 같이 업데이트하기.
+      // 사진이 있든 없든 사진도 변경할 수 있도록 하기.
+      // 사진 존재하면 업데이트된 사진으로 변경하기.
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsEditing(false);
+    }
+  };
   const onDelete = async () => {
     const ok = confirm("Are you sure you want to delete this tweet?");
 
@@ -68,8 +157,38 @@ export default function Tweet({ userName, photo, tweet, userId, id }: ITweet) {
     <Wrapper>
       <Column>
         <UserName>{userName}</UserName>
-        <Payload>{tweet}</Payload>
-        {user?.uid === userId ? <DeleteButton onClick={onDelete}>Delete</DeleteButton> : null}
+        {isEditing ? (
+          <EditTweetFormTextArea
+            rows={5}
+            maxLength={180}
+            onChange={onChange}
+            placeholder={tweet}
+            value={editedTweet}
+          ></EditTweetFormTextArea>
+        ) : (
+          <Payload>{tweet}</Payload>
+        )}
+
+        {
+          // 작성자본인인지?
+          // isEditing이 활성화되어있는지? ->
+          user?.uid === userId ? (
+            <>
+              {isEditing ? (
+                <>
+                  <CancelButton onClick={handleCancel}>Cancel</CancelButton>
+                  <UpdateButton onClick={onUpdate}>Update</UpdateButton>
+                </>
+              ) : (
+                <EditButton onClick={handleEdit}>Edit</EditButton>
+              )}
+            </>
+          ) : null
+        }
+        {
+          // 삭제 버튼
+          user?.uid === userId ? <DeleteButton onClick={onDelete}>Delete</DeleteButton> : null
+        }
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
